b0fb7fd8917df1b038d8a6bf9795bb2e7e28ee34
unknown <core.maildesu@gmail.com>
2024-02-24 12:57:07 +0900

    add

--- js/header.js
+++ js/header.js
@@ -1,16 +1,4 @@
 $(function() {
-    var pos = $("nav").offset().top;
-    
-    var height = $("header").outerHeight();
-
-    $(window).scroll(function(){
-
-        if($(this).scrollTop() >= pos){
-            $("nav").addClass("fixed");
-            $("body").css("margin-top", height);
-        }else{
-            $("nav").removeClass("fixed");
-            $("body").css("margin-top", 0);
-        }
-    });
+    // var height=$("header").height();
+    // $("body").css("margin-top", height + 50);
 });
